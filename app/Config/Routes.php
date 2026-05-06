6e7231a0c1b0d528984fd8279ee2d58772af048b
georgeet15 <georgeemmanuelthomas@gmail.com>
2026-05-06 15:30:04 +0530

    feat: update default route to point to Login controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Login::index');
 
 // Auth routes
 $routes->get('/login', 'Login::index');
